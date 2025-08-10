508d2f211cc0d83bec64b41bc51671619c81b05b
serasmecpt024 <serasmecpt024@student.wethinkcode.co.za>
2025-08-10 23:16:42 +0200

    added an error handling for invalid inputs

--- Guess_Number.py
+++ Guess_Number.py
@@ -2,13 +2,17 @@ attempts= 0
 
 secrect_number = 3
 
-Guessed_number = int(input("Please enter a number : "))
+while True:
+    try: 
+        
+        Guessed_number = int(input("Please enter a number : "))
+        attempts += 1
 
-if Guessed_number != secrect_number:
-    print("Sorry try again")
-elif Guessed_number > secrect_number:
-    print ("You guessed too high")
-elif Guessed_number < secrect_number:
-    print (" You guessed too low")
-else:
-    print ("Congratulations You have WON!")
+        if Guessed_number > secrect_number:
+            print ("You guessed too high")
+        elif Guessed_number < secrect_number:
+            print (" You guessed too low")
+        else:
+            print ("Congratulations You have WON!")
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
